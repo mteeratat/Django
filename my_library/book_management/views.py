1e164e52c76e2759b7cad5a276f23d0c9fc8c36b
M CPE <mmmiko@hotmail.com>
2021-11-10 00:25:03 +0700

    finish

--- my_library/book_management/views.py
+++ my_library/book_management/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponseGone
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import context, loader
+from book_management.models import Category, Book
 
 # Create your views here.
 
@@ -17,9 +18,16 @@ from django.template import context, loader
 #     }
 #     return HttpResponse(template.render(context, request))
 
+# def index(request):
+#     header_str = 'Hello, Python variable'
+#     context = {
+#         'var1': header_str
+#     }
+#     return render(request, 'index.html', context)
+
 def index(request):
-    header_str = 'Hello, Python variable'
     context = {
-        'var1': header_str
+        'cat_num': len(Category.objects.all()),
+        'book_num': len(Book.objects.all())
     }
-    return render(request, 'index.html', context)
+    return render (request, 'index.html', context)
